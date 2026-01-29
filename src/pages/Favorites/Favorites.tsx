4c4cdad59b78f9561ff36ee82bdda575882355db
kostryginden <kostryginden@gmail.com>
2026-01-29 18:49:27 +0300

    feat: меняет моковые данные на данные из store для раздела "Избранное" 5-redux

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,61 +1,14 @@
 import Title from "../../components/Title/Title";
 import styles from "./styles.module.css";
 
+import { useSelector } from "react-redux";
 import FilmList from "../../components/FilmList/FilmList";
-
-const filmsList = [
-	{
-		name: "Black Widow",
-		poster: "black_widow.png",
-		likes: 324,
-		isFavorit: true,
-	},
-	{
-		name: "Shang Chi",
-		poster: "shang_chi.png",
-		likes: 124,
-		isFavorit: false,
-	},
-	{
-		name: "Loki",
-		poster: "loki.png",
-		likes: 235,
-		isFavorit: false,
-	},
-	{
-		name: "How I Met Your Mother",
-		poster: "How_I_Met_Your_Mother.png",
-		likes: 123,
-		isFavorit: false,
-	},
-	{
-		name: "Money Heist",
-		poster: "Money_Heist.png",
-		likes: 8125,
-		isFavorit: true,
-	},
-	{
-		name: "Friends",
-		poster: "Friends.png",
-		likes: 123,
-		isFavorit: false,
-	},
-	{
-		name: "The Big Bang Theory",
-		poster: "The_Big_Bang_Theory.png",
-		likes: 12,
-		isFavorit: false,
-	},
-	{
-		name: "Two And a Half Men",
-		poster: "Two_And_a_Half_Men.png",
-		likes: 456,
-		isFavorit: false,
-	},
-];
+import { RootState } from "../../store/store";
 
 export const Favorites = () => {
-	const favoritefilms = filmsList.filter(film => film.isFavorit);
+	const favoritefilms = useSelector(
+		(store: RootState) => store.favorite.movies,
+	);
 
 	return (
 		<div className={styles["favorites-root"]}>
